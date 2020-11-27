b3b0dc572cb60c60b30b43d2dd7b6d83201f01be
Louis <louis.ricour@gmail.com>
2020-11-27 14:35:43 +0100

    add decoupeAttribut function

--- readXml.c
+++ readXml.c
@@ -64,3 +64,61 @@ void lecturecontent(FILE *f, struct Tag *tab){ // lecture des donnée jusque det
     strcpy(tab->content ,content);
 
 }
+
+void decoupeAttribut(FILE *f, char *nomBalise ,struct Tag *tab, int longeur) {
+    char nomAtt[30];
+    char valeurAtt[30];
+    int compteur = 0;
+    int nombreAtt = 0;
+    struct Attr tempo[10];
+
+    int i = 0;
+    int fin = 0;
+    while (fin == 0|| i<longeur){
+        while (nomBalise[i] != '=') {
+            if (nomBalise[i] == '\0') {
+                fin = 1;
+            } else {
+                i++;
+                nomAtt[compteur] = nomBalise[i];
+                compteur++;
+            }
+        }
+        nomAtt[compteur-1]='\0';
+        compteur = 0;
+        while (nomBalise[i] != ' ') {
+            if (nomBalise[i] == '\0' || nomBalise[i]=='>') {
+                fin = 1;
+                break;
+            } else {
+                i++;
+                valeurAtt[compteur] = nomBalise[i];
+                compteur++;
+            }
+        }
+        valeurAtt[compteur]='\0';
+        compteur = 0;
+        struct Attr monAttr;
+        // monAttr.name = nomAtt;
+        //monAttr.value = valeurAtt;
+
+        char *test = nomAtt;
+        char  *teeest = valeurAtt;
+        tempo[nombreAtt].name= (char *) test;
+        tempo[nombreAtt].value= (char *) teeest;
+        nombreAtt++;
+
+    }
+//
+    tab->attributs = malloc(sizeof( Attr )* nombreAtt);
+    for (int y=0; y<nombreAtt;y++)
+    {
+        struct Attr *attribut = malloc(sizeof(Attr));
+        attribut->name = malloc(sizeof(char) * strlen(tempo[y].name));
+        strcpy(attribut->name, tempo[y].name);
+        attribut->value = malloc(sizeof(char) * strlen(tempo[y].value));
+        strcpy(attribut->value, tempo[y].value);
+        tab->attributs[y] = attribut;
+    }
+    tab->attributsAmount= nombreAtt;
+}
